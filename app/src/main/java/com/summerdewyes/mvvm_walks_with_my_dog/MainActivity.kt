055d54bd00a28873153b23640b9bb598123263bd
Dew <summerdewyes@gmail.com>
2021-09-06 14:18:45 +0900

    Initial commit

---
+++ app/src/main/java/com/summerdewyes/mvvm_walks_with_my_dog/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.summerdewyes.mvvm_walks_with_my_dog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
