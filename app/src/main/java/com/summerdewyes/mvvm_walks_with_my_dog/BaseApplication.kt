e56dc7f36fe5ce4dc932ceeb9273ccabeee6235c
Dew <summerdewyes@gmail.com>
2021-10-01 20:25:25 +0900

    Dagger Hilt 공부중..

--- app/src/main/java/com/summerdewyes/mvvm_walks_with_my_dog/BaseApplication.kt
+++ app/src/main/java/com/summerdewyes/mvvm_walks_with_my_dog/BaseApplication.kt
@@ -6,13 +6,13 @@ import timber.log.Timber
 
 /**
  * @HiltAndroidApp에 의해서 SingletonComponent가 생성된다.
- * 앱이 살아있는 동안 Dependency를 제공하는 역할
+ * 앱이 살아있는 동안 Dependency를 제공하는 역할.
  */
 @HiltAndroidApp
 class BaseApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        Timber.plant(Timber.DebugTree()) // 태그를 생략하고 로그를 찍을 수 있는 라이브러리입니다.
     }
 }
--- app/src/main/java/com/summerdewyes/mvvm_walks_with_my_dog/di/AppModule.kt
+++ app/src/main/java/com/summerdewyes/mvvm_walks_with_my_dog/di/AppModule.kt
@@ -20,13 +20,18 @@ import javax.inject.Singleton
 
 
 /**
- * 제일 처음 실행 됨
+ * 제일 처음 실행 됩니다.
+ * BaseApplication.kt로 종속성을 주입하고 싶다고 앱에 알리고 실제로 수행 할 AppModule을 만들었습니다.
  */
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(SingletonComponent::class) // 앱 모듈 내부의 객체가 생성되는 시점과 소멸되는 시점을 결정합니다. SingletonComponent는 앱의 전체 수명동안 존재하고 앱이 다시 파괴 될 때 함께 파괴됩니다.
 object AppModule {
 
+    // @Singleton 범위 지정으로 SingletonComponent의 인스턴스 당 한 번만 생성되며, 해당 바인딩에 대한 모든 요청은 동일한 인스턴스를 공유합니다.
+
+    // 해당 데이터베이스를 사용할 클래스에 대해 실행 중인 데이터베이스를 생성합니다.
+    // Dagger는 Context를 어디에서 가져와야 하는지 알지 못하기 때문에 @ApplicationContext 주석을 추가하여 해결할 수 있습니다.
     @Singleton
     @Provides
     fun provideRunningDatabase(@ApplicationContext app: Context) =
@@ -34,6 +39,7 @@ object AppModule {
         RUNNING_DATABASE_NAME
     ).build()
 
+    // 실행 중인 데이터베이스를 생성하는 방법을 알고 있는 경우에만 Dao를 생성할 수 있습니다.
     @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
